6a3cb56faf87ab45c771a4f249c921b73f15f4b1
Hasti Ahoei <97366164+Hastiahoei@users.noreply.github.com>
2022-07-06 11:08:10 +0200

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,72 @@
+const prev = document.querySelector('#left');
+const next = document.querySelector('#right');
+const slides = document.querySelectorAll('.slide');
+let auto = true;
+let interval = 3000;
+let initSlide;
+
+next.addEventListener('click', function(){
+	nextSlide();
+	clearInterval(initSlide);
+	initSlide = setInterval(nextSlide, interval); //to reset timeout
+});
+
+prev.addEventListener('click', function(){
+	prevSlide();
+	clearInterval(initSlide);
+	initSlide = setInterval(nextSlide, interval); //to reset timeout
+});
+
+const nextSlide = () => {
+	let current = document.querySelector('.current');
+	current.classList.remove('current');
+
+	if (current.nextElementSibling) {
+		current.nextElementSibling.classList.add('current');
+	}
+	else{
+		slides[0].classList.add('current');
+	}
+
+	setTimeout(() => current.classList.remove('current'));
+}
+
+const prevSlide = () => {
+	let current = document.querySelector('.current');
+	current.classList.remove('current');
+
+	if (current.previousElementSibling) {
+		current.previousElementSibling.classList.add('current');
+	}
+	else{
+		slides[slides.length - 1].classList.add('current');
+	}
+
+	setTimeout(() => current.classList.remove('current'));
+}
+
+
+
+
+
+/*document.getElementById("stopAuto").addEventListener("click", ()=>{
+	clearInterval(interval);
+});*/
+
+const stopAuto =document.getElementById("stopAuto")
+
+stopAuto.addEventListener("click" , function () {
+	auto =!auto
+	console.log(auto);
+}
+)
+
+
+if (auto ) {
+	initSlide = setInterval(nextSlide, interval);
+}
+
+else {
+	clearInterval(initSlide)
+}
+
