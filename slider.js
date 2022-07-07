eb8c3f6b7a749efd06b89c3ab9ad9726f5393634
Hasti Ahoei <97366164+Hastiahoei@users.noreply.github.com>
2022-07-07 10:30:51 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -5,45 +5,48 @@ let auto = true;
 let interval = 3000;
 let initSlide;
 
-next.addEventListener('click', function(){
+next.addEventListener("click", function(){
 	nextSlide();
 	clearInterval(initSlide);
 	initSlide = setInterval(nextSlide, interval); //to reset timeout
 });
 
-prev.addEventListener('click', function(){
+prev.addEventListener("click", function(){
 	prevSlide();
 	clearInterval(initSlide);
 	initSlide = setInterval(nextSlide, interval); //to reset timeout
 });
 
+
 const nextSlide = () => {
-	let current = document.querySelector('.current');
-	current.classList.remove('current');
+	let current = document.querySelector(".current");
+	current.classList.remove("current");
 
 	if (current.nextElementSibling) {
-		current.nextElementSibling.classList.add('current');
+		current.nextElementSibling.classList.add("current");
 	}
 	else{
-		slides[0].classList.add('current');
+		slides[0].classList.add("current");
 	}
 
-	setTimeout(() => current.classList.remove('current'));
+	setTimeout(() => current.classList.remove("current"));
 }
+
 
 const prevSlide = () => {
-	let current = document.querySelector('.current');
-	current.classList.remove('current');
+	let current = document.querySelector(".current");
+	current.classList.remove("current");
 
 	if (current.previousElementSibling) {
-		current.previousElementSibling.classList.add('current');
+		current.previousElementSibling.classList.add("current");
 	}
 	else{
-		slides[slides.length - 1].classList.add('current');
+		slides[slides.length - 1].classList.add("current");
 	}
 
-	 setTimeout(() => current.classList.remove('current'));
+	setTimeout(() => current.classList.remove("current"));
 }
+
 
 
 if (auto ) {
